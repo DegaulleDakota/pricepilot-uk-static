7618f0c9c10eaa7e79946b693e8b8dad595cd4bd
Degaulle Dakota <dukesnr@yahoo.co.uk>
2025-10-07 07:03:21 -0700

    Connect working DigitalOcean function to frontend

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,7 @@
-// src/lib/api.js 
 export async function searchProducts(query) {
-  const url = new URL("https://faas-fra1-afec6ce7.doserverless.co/api/v1/web/fn-f90ec0fb-42a4-4b82-9417-be5790de6095/api/search");
+  const url = new URL(
+    "https://faas-fra1-afec6ce7.doserverless.co/api/v1/web/fn-f90ec0fb-42a4-4b82-9417-be5790de6095/api/search"
+  );
   url.searchParams.set("q", query.trim());
 
   const r = await fetch(url, { headers: { accept: "application/json" } });
