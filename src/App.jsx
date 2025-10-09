b4bffac39f000e81f621414fd1166f79fd08aaf9
Degaulle Dakota <dukesnr@yahoo.co.uk>
2025-10-09 07:26:56 -0700

    Wire routes for Home/About/Privacy/Terms

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import About from "./pages/About";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
 
 export default function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<TermsOfService />} />
+
+        {/* Optional: catch-all to Home */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
